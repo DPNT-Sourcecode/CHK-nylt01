00d988cbb61df70bf9093942ed7891a0ae67e8aa
sbx_user1051 <sbx_user1051@169.254.151.249>
2022-05-20 18:30:48 +0000

    Fri May 20 18:30:48 UTC 2022

--- lib/solutions/CHK/teller.py
+++ lib/solutions/CHK/teller.py
@@ -5,10 +5,10 @@ from typing import List
 class Teller:
     def __init__(self, pricingRules:List[dict]):
         self.pricers = pricing.PricerFactory().get_pricers(pricingRules)
-        self.bundleBasket = defaultdict(int)
+        self.bundlePricerBasket = defaultdict(int)
 
     def add_item(self, item:str) -> None:
-        self.bundleBasket[item] += 1
+        self.bundlePricerBasket[item] += 1
         self.pricers[item].increment()
     
     def calculate_total(self, skus:str) -> int:
@@ -18,7 +18,7 @@ class Teller:
         runningTotal = 0
         for pricer in self.pricers.values():
             if isinstance(pricer, pricing.BundlePricer): 
-                total, self.bundleBasket = pricer.total(self.bundleBasket)
+                total, self.bundlePricerBasket = pricer.total(self.bundlePricerBasket)
                 runningTotal += total
             else:
                 runningTotal += pricer.total()
