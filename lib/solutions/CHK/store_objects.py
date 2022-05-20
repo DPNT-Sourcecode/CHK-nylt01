29eae5e16775a6d3f5d3a929e9d0d6343e4554bf
sbx_user1051 <sbx_user1051@169.254.151.249>
2022-05-20 20:06:49 +0000

    Fri May 20 20:06:49 UTC 2022

--- lib/solutions/CHK/store_objects.py
+++ lib/solutions/CHK/store_objects.py
@@ -2,10 +2,10 @@ from enum import Enum, auto
 
 pricingRules = [
         {'item': 'A', 'unitPrice': 50, 'special': [(5, 200), (3, 130)]}, 
-        {'item': 'B', 'unitPrice': 30, 'special': [(2, 45)], 'bundle': {'requirement': {'E': 2, 'B': 1}, 'discount': 30}}, 
+        {'item': 'B', 'unitPrice': 30, 'special': [(2, 45)], 'bundleID': 1}, 
         {'item': 'C', 'unitPrice': 20}, 
         {'item': 'D', 'unitPrice': 15},
-        {'item': 'E', 'unitPrice': 40, 'bundle': {'requirement': {'E': 2, 'B': 1}, 'discount': 30}},
+        {'item': 'E', 'unitPrice': 40, 'bundleID': 1},
         {'item': 'F', 'unitPrice': 10, 'special': [(3, 20)]},
         {'item': 'G', 'unitPrice': 20},
         {'item': 'H', 'unitPrice': 10, 'special': [(5, 45), (10, 80)]},
@@ -13,23 +13,31 @@ pricingRules = [
         {'item': 'J', 'unitPrice': 60},
         {'item': 'K', 'unitPrice': 70, 'special': [(2, 120)]},
         {'item': 'L', 'unitPrice': 90},
-        {'item': 'M', 'unitPrice': 15, 'bundle': {'requirement': {'N': 3, 'M': 1}, 'discount': 15}},
-        {'item': 'N', 'unitPrice': 40, 'bundle': {'requirement': {'N': 3, 'M': 1}, 'discount': 15}},
+        {'item': 'M', 'unitPrice': 15, 'bundleID': 2},
+        {'item': 'N', 'unitPrice': 40, 'bundleID': 2},
         {'item': 'O', 'unitPrice': 10},
         {'item': 'P', 'unitPrice': 50, 'special': [(5, 200)]},
-        {'item': 'Q', 'unitPrice': 30, 'special': [(3, 80)], 'bundle': {'requirement': {'R': 3, 'Q': 1}, 'discount': 30}},
-        {'item': 'R', 'unitPrice': 50, 'bundle': {'requirement': {'R': 3, 'Q': 1}, 'discount': 30}},
-        {'item': 'S', 'unitPrice': 20, 'group': {'groupMembers': 'STXYZ', 'groupQuantity': 3, 'groupPrice': 45}},
-        {'item': 'T', 'unitPrice': 20, 'group': {'groupMembers': 'STXYZ', 'groupQuantity': 3, 'groupPrice': 45}},
+        {'item': 'Q', 'unitPrice': 30, 'special': [(3, 80)], 'bundleID': 3},
+        {'item': 'R', 'unitPrice': 50, 'bundleID': 3},
+        {'item': 'S', 'unitPrice': 20, 'groupID': 1},
+        {'item': 'T', 'unitPrice': 20, 'groupID': 1},
         {'item': 'U', 'unitPrice': 40, 'special': [(4, 120)]},
         {'item': 'V', 'unitPrice': 50, 'special': [(2, 90), (3, 130)]},
         {'item': 'W', 'unitPrice': 20},
-        {'item': 'X', 'unitPrice': 17, 'group': {'groupMembers': 'STXYZ', 'groupQuantity': 3, 'groupPrice': 45}},
-        {'item': 'Y', 'unitPrice': 20, 'group': {'groupMembers': 'STXYZ', 'groupQuantity': 3, 'groupPrice': 45}},
-        {'item': 'Z', 'unitPrice': 21, 'group': {'groupMembers': 'STXYZ', 'groupQuantity': 3, 'groupPrice': 45}}
+        {'item': 'X', 'unitPrice': 17, 'groupID': 1},
+        {'item': 'Y', 'unitPrice': 20, 'groupID': 1},
+        {'item': 'Z', 'unitPrice': 21, 'groupID': 1}
     ]
 
+bundledict = {
+    1: {'requirement': {'E': 2, 'B': 1}, 'discount': 30},
+    2: {'requirement': {'N': 3, 'M': 1}, 'discount': 15},
+    3: {'requirement': {'R': 3, 'Q': 1}, 'discount': 30}
+}
 
+groupdict = {
+    1: {'groupMembers': 'STXYZ', 'groupQuantity': 3, 'groupPrice': 45}
+}
 
 class Products(Enum):
     @classmethod
@@ -63,5 +71,6 @@ class Products(Enum):
     X = auto()
     Y = auto()
     Z = auto()
+
 
 
